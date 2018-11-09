225600d8dd59b09835a69619ee0f2eee0eee75f2
alexandre <alex.ayato@gmail.com>
2018-11-09 10:22:34 +0100

    test message

---
+++ HelloWorld.java
@@ -0,0 +1,95 @@
+package com.exo;
+
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Exercice 1");
+		int varA = 0,varB = 0,varC = 0, varT = 0;
+		varA = 1;
+		varB = varA + 3;
+		varA = 3;
+				
+		System.out.println("A = " + varA);
+		System.out.println("B = " + varB);
+		
+		
+		System.out.println("Exercice 2");
+		varA = 5;
+		varB = 3;
+		varC = varA + varB;
+		varA = 2;
+		varC = varB - varA;
+				
+		System.out.println("A = " + varA);
+		System.out.println("B = " + varB);
+		System.out.println("C = " + varC);
+		
+		System.out.println("Exercice 3");
+		varA = 5;
+		varB = varA + 4;
+		varA = varA + 1;
+		varB = varA - 4;
+		
+				
+		System.out.println("A = " + varA);
+		System.out.println("B = " + varB);
+
+		
+		System.out.println("Exercice 4");
+		varA = 3;
+		varB = 10;
+		varC = varA + varB;
+		varB = varA + varB;
+		varA = varC;
+				
+		System.out.println("A = " + varA);
+		System.out.println("B = " + varB);
+		System.out.println("C = " + varC);
+		
+		System.out.println("Exercice 5");
+		varA = 5;
+		varB = 2;
+		varA = varB;
+		varB = varA;
+				
+		System.out.println("A = " + varA);
+		System.out.println("B = " + varB);
+		
+		System.out.println("Exercice 6");
+		varA = 100;
+		varB = 15;
+		varC = 30;
+		varT = varA;
+		varA = varB;
+		varB = varT;
+				
+		System.out.println("A = " + varA);
+		System.out.println("B = " + varB);
+		
+		System.out.println("Exercice 7");
+		varT = varA;
+		varA = varC;
+		varC = varB;
+		varB = varT;
+				
+		System.out.println("A = " + varA);
+		System.out.println("B = " + varB);
+		System.out.println("C = " + varC);
+		
+		System.out.println("Exercice 8");
+		String varD = "423",varE = "12";
+		
+				
+		System.out.println(varD + varE);
+
+		
+		System.out.println("Exercice 9");
+		
+				
+		System.out.println(varD + varE);
+		
+
+	}
+	
+}
